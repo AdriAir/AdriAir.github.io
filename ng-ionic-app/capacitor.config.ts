124c13c117b5fe0c0c1451845815b592d8c03319
AdriAir <aborio2004@gmail.com>
2024-07-13 16:32:49 +0200

    chore: ionic-angular app init

---
+++ ng-ionic-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ng-ionic-app',
+  webDir: 'www'
+};
+
+export default config;
